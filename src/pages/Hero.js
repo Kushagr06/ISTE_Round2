e6b24148e7703966af640e972018df68b96ee1a6
Kushagr Narayan <nkushagr@gmail.com>
2023-02-19 16:23:48 +0530

    heroPage

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -14,7 +14,7 @@ function Hero() {
      <div className=" align-middle h-auto text-center z-2 mt-2 mb-10 pb-6 bg-[rgb(34,34,34)]">
       <Blimpg />
     <div className='m-2'>
-    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4  border-blue-500 hover:border-transparent rounded">
               <h1  id="title_hero" className='pb-5 pt-10 mb-5'> Welcome Explorers! </h1>
       </button>
      
