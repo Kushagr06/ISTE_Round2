58acbe968092ebe18c5d5701aa03ebdfd16f899f
Kushagr Narayan <nkushagr@gmail.com>
2023-02-19 17:44:20 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './pages/Hero';
-<<<<<<< HEAD
-function App() {
-  return (
-    <div className='bg-[rgb(34,34,34)] overflow-hidden'>
-      <Navbar/>
-=======
 import News from './pages/News';
+
 
 function App() {
   return (
