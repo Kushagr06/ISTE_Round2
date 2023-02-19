890c822c946b905c4282ed21532abcdc6c2a6a04
Kushagr Narayan <nkushagr@gmail.com>
2023-02-19 17:29:07 +0530

    searchbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './pages/Hero';
+import News from './pages/News';
 
 function App() {
   return (
-    <div className='bg-[rgb(34,34,34)] overflow-hidden'>
+    <div className='bg-[rgb(34,34,34)] '>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
+      <News />
     </div>
   
   );
