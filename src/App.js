3bc12bd011d5498f353f63cbd2a09d7ec16ff42a
Kushagr Narayan <nkushagr@gmail.com>
2023-02-19 18:13:35 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Card from './components/Card';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Hero from './pages/Hero';
 import News from './pages/News';
@@ -9,7 +11,11 @@ function App() {
     <div className='bg-[rgb(34,34,34)] '>
       <Navbar />
       {/* <Hero /> */}
+      <Card />
       <News />
+
+      {/* <Footer /> */}
+
     </div>
   
   );
